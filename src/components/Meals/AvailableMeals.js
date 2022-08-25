230b8f2813cc3ac7d9c9b70ca8cee07862cc6ad2
Mehmet <mathdenizi@gmail.com>
2022-08-25 20:23:08 +0200

    fetch Error messages added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,17 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://thefoodorder5180-default-rtdb.firebaseio.com/meals.json"
+        "https://thefoodorder5180-default-rtdb.firebaseio.com/meals"
       );
+
+      if (!response.ok) {
+        throw new Error("smtg went wrong");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,7 +35,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   const mealsList = meals.map((meal) => (
@@ -46,6 +55,14 @@ const AvailableMeals = () => {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
