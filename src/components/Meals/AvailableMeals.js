a82a45304af1322fd237235b68e9a40b4a3ec1b5
Mehmet <mathdenizi@gmail.com>
2022-08-26 20:18:51 +0200

    simple form component added to cart component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://thefoodorder5180-default-rtdb.firebaseio.com/meals"
+        "https://thefoodorder5180-default-rtdb.firebaseio.com/meals.json"
       );
 
       if (!response.ok) {
